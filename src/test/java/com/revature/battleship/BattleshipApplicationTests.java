de1b2bdd168363871f0ef033580e0583ea9bb9f1
Gautam <gramasu000@citymail.cuny.edu>
2020-07-14 11:26:04 -0400

    Spring Boot initial build

---
+++ src/test/java/com/revature/battleship/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
