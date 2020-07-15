c52cc38058887693081935f052bd9fd04a6f5dfa
Gautam <gramasu000@citymail.cuny.edu>
2020-07-15 14:12:43 -0400

    Added application.yml and moved mongodb uri to envionment variable

--- src/main/java/com/revature/battleship/BattleshipApplication.java
+++ src/main/java/com/revature/battleship/BattleshipApplication.java
@@ -2,6 +2,10 @@ package com.revature.battleship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class BattleshipApplication {
@@ -9,5 +13,13 @@ public class BattleshipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BattleshipApplication.class, args);
 	}
+  
+  @Bean
+  ApplicationRunner applicationRunner(Environment environment) {
+    return args -> {
+      System.out.println(environment.getProperty("spring.data.mongodb.uri"));
+    };
+  }
+
 
 }
